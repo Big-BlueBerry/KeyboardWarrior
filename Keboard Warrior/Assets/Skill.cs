cd37e139f268142b16ec65f262a50a55f0b50225
Ki-Hin <in010210@gmail.com>
2017-09-21 22:32:41 +0900

    스킬 시스템 기반 구현

---
+++ Keboard Warrior/Assets/Skill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    delegate void SkillDelegate(int n);
+
+    List<SkillDelegate> SkillList = new List<SkillDelegate> { };
+
+    Dictionary<string, SkillDelegate> SkillDictionary = new Dictionary<string, SkillDelegate>();
+    SkillDictionary.Add("firebress", Skill.Firebress);
+    
+    SkillDelegate input = SkillDictionary["icebress"];
+
+    public void Icebress(int n)
+    {
+
+    }
+    public void Firebress(int n)
+    {
+
+    }
+}
